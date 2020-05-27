4904b264125f99720913ed1399cd331070d18a0c
msabareesh <sabareesh.madambath@gmail.com>
2020-05-27 21:47:20 +0530

    First Version
    Signed-off-by: msabareesh <sabareesh.madambath@gmail.com>

---
+++ src/profiles/models.py
@@ -0,0 +1,13 @@
+
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+
+
+class profile(models.Model):
+    name = models.CharField(max_length=120)
+    desc = models.TextField(default="Description Default Text")
+
+    def __unicode__(self):
+        return self.name
