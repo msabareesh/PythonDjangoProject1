21091e8f3a6f325c9adcbfeb9e0cc9cb0a60b103
msabareesh <sabareesh.madambath@gmail.com>
2020-05-28 01:01:16 +0530

    May 27 - EOD
    Signed-off-by: msabareesh <sabareesh.madambath@gmail.com>

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
--- src/tryTen/urls.py
+++ src/tryTen/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+
+from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
